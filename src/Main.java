6e49dfe8034ab20b8f4e5580edde9fd8bfcd0518
Curtis Buswell <buswelca@mail.uc.edu>
2023-06-07 13:01:06 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2;
+        int EVEN_OR_ODD_CHECKER = 2;
+        int evenOrOddResult; evenOrOddResult = numToExamine % EVEN_OR_ODD_CHECKER;
+        if (evenOrOddResult == 0)
+            System.out.println("Your number, " + numToExamine + ", is even.");
+        else
+            System.out.println("Your number, " + numToExamine + ", is odd.");
+    }
+}
+
---
+++ src/Number.java
@@ -0,0 +1,4 @@
+public class Number {
+    int numToExamine = 2;
+
+}
